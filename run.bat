2867a97cfca1791a5b480a4a1e2804a390f17e0c
juanfraa431 <juanfraa032@gmail.com>
2025-06-10 21:08:30 -0300

    hola

--- run.bat
+++ run.bat
@@ -3,15 +3,25 @@ echo Instalando dependencias y ejecutando servidores...
 
 cd Back
 if not exist node_modules (
-  npm install
+  echo Instalando dependencias del BACKEND...
+  npm install || goto error
 )
 cd ..
 
 cd Front
 if not exist node_modules (
-  npm install
+  echo Instalando dependencias del FRONTEND...
+  npm install || goto error
 )
 cd ..
 
-npx concurrently "npm run startBack --prefix Back" "npm run startFront --prefix Front"
+echo Levantando servidores...
+npx concurrently "npm run startBack --prefix Back" "npm run startFront --prefix Front" || goto error
+
 pause
+exit
+
+:error
+echo ❌ Ocurrió un error. Revisá si tenés Node.js instalado correctamente.
+pause
+exit
